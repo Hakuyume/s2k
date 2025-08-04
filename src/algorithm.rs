65b368ad2510a3a28466cfffeaaf801423035578
Toru Ogawa <Hakuyume@users.noreply.github.com>
2025-08-04 17:12:07 +0900

    drop s2k

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -1,5 +1,4 @@
 use base64::prelude::{BASE64_STANDARD, Engine};
-use sha2::{Sha256, digest};
 
 #[derive(
     Clone,
@@ -17,10 +16,10 @@ pub(crate) enum Algorithm {
     Argon2id256,
     #[strum(message = "argon2id (512 bits)")]
     Argon2id512,
+    #[strum(message = "argon2id (4 digits)")]
+    Argon2id4,
     #[strum(message = "argon2id (6 digits)")]
     Argon2id6,
-    #[strum(message = "s2k (SHA256)")]
-    S2kSha256,
 }
 
 impl Algorithm {
@@ -47,42 +46,33 @@ impl Algorithm {
                     .hash_password_into(password.as_ref(), salt.as_ref(), &mut key)
                     .map(|_| BASE64_STANDARD.encode(key))
             }
+            Algorithm::Argon2id4 => {
+                let mut key = [0u8; 2];
+                argon2
+                    .hash_password_into(password.as_ref(), salt.as_ref(), &mut key)
+                    .map(|_| format!("{:04}", u16::from_be_bytes(key) % 10_000))
+            }
             Algorithm::Argon2id6 => {
                 let mut key = [0u8; 4];
                 argon2
                     .hash_password_into(password.as_ref(), salt.as_ref(), &mut key)
                     .map(|_| format!("{:06}", u32::from_be_bytes(key) % 1_000_000))
             }
-            Algorithm::S2kSha256 => {
-                Ok(BASE64_STANDARD.encode(s2k::<Sha256>(salt.as_ref(), password.as_ref(), 65536)))
-            }
         }
     }
 }
 
-fn s2k<D>(salt: &[u8], passphrase: &[u8], count: usize) -> digest::Output<D>
-where
-    D: digest::Digest,
-{
-    let mut hasher = D::new();
-    for &b in salt.iter().chain(passphrase).cycle().take(count) {
-        hasher.update([b]);
-    }
-    hasher.finalize()
-}
-
 #[cfg(test)]
 mod tests {
-    use hex_literal::hex;
-    use sha2::Sha256;
+    use argon2::Argon2;
 
     #[test]
-    fn test_s2k() {
-        // gpg --cipher-algo AES256 --s2k-count 65536 --s2k-digest SHA256 --s2k-mode 3 --symmetric <FILE>
-        // gpg --list-packets --show-session-key <FILE>.gpg
+    fn test_argon2id256() {
         assert_eq!(
-            super::s2k::<Sha256>(&hex!("3109800B39D9C9D6"), b"passphrase", 65536).as_slice(),
-            hex!("4892EE6C021A36201DE80C625C7F2B654C3AAC4578308F03A22B67BF25E893F6"),
+            super::Algorithm::Argon2id256
+                .key(&Argon2::default(), "password", "salt2025")
+                .unwrap(),
+            "koBvTFMBiW3E247iA86fq//8WZrOb8jUWXstei0b5NY=",
         );
     }
 }
