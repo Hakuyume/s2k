9266d896d7e4d798fe1132b1325f758688068493
Toru Ogawa <Hakuyume@users.noreply.github.com>
2026-05-23 12:00:26 +0900

    define Chars trait

--- src/s2k.rs
+++ src/s2k.rs
@@ -41,25 +41,25 @@ impl Algorithm {
                 let mut key = [0u8; Params::DEFAULT_OUTPUT_LEN];
                 argon2
                     .hash_password_into(password.as_ref(), salt.as_ref(), &mut key)
-                    .map(|_| encode(&key, '0'..='9', 4))
+                    .map(|_| encode(&key, &Digits(10), 4))
             }
             Algorithm::Argon2id6 => {
                 let mut key = [0u8; Params::DEFAULT_OUTPUT_LEN];
                 argon2
                     .hash_password_into(password.as_ref(), salt.as_ref(), &mut key)
-                    .map(|_| encode(&key, '0'..='9', 6))
+                    .map(|_| encode(&key, &Digits(10), 6))
             }
         }
     }
 }
 
-fn encode<C>(key: &[u8], chars: C, count: usize) -> String
+fn encode<C>(key: &[u8], chars: &C, count: usize) -> String
 where
-    C: Clone + IntoIterator<Item = char>,
+    C: Chars,
 {
     let mut remainder = vec![0; count];
 
-    let n = chars.clone().into_iter().count() as u32;
+    let n = chars.len();
     for b in key {
         let mut b = *b as u32;
         for r in remainder.iter_mut().rev() {
@@ -71,8 +71,24 @@ where
 
     remainder
         .into_iter()
-        .map(|r| chars.clone().into_iter().nth(r as _).unwrap())
+        .map(|r| chars.get(r).unwrap())
         .collect()
+}
+
+trait Chars {
+    fn len(&self) -> u32;
+    fn get(&self, index: u32) -> Option<char>;
+}
+
+struct Digits(u32);
+impl Chars for Digits {
+    fn len(&self) -> u32 {
+        self.0
+    }
+
+    fn get(&self, index: u32) -> Option<char> {
+        char::from_digit(index, self.0)
+    }
 }
 
 #[cfg(test)]
@@ -101,28 +117,29 @@ mod tests {
 
     #[test]
     fn test_encode() {
-        assert_eq!(
-            super::encode(b"key", (u8::MIN..=u8::MAX).map(char::from), 6),
-            "\0\0\0key",
-        );
+        struct U8;
+        impl super::Chars for U8 {
+            fn len(&self) -> u32 {
+                1 << 8
+            }
+
+            fn get(&self, index: u32) -> Option<char> {
+                u8::try_from(index).ok().map(Into::into)
+            }
+        }
+        assert_eq!(super::encode(b"key", &U8, 6), "\0\0\0key",);
 
         // python -c 'print(str(int.from_bytes(b"key"))[-6:])'
-        assert_eq!(super::encode(b"key", '0'..='9', 6), "038329");
+        assert_eq!(super::encode(b"key", &super::Digits(10), 6), "038329");
 
         // python -c 'print(bin(int.from_bytes(b"key"))[-6:])'
-        assert_eq!(super::encode(b"key", '0'..='1', 6), "111001");
+        assert_eq!(super::encode(b"key", &super::Digits(2), 6), "111001");
         // python -c 'print(oct(int.from_bytes(b"key"))[-6:])'
-        assert_eq!(super::encode(b"key", '0'..='7', 6), "662571");
+        assert_eq!(super::encode(b"key", &super::Digits(8), 6), "662571");
         // python -c 'print(hex(int.from_bytes(b"key"))[-6:])'
-        assert_eq!(
-            super::encode(b"key", ('0'..='9').chain('a'..='f'), 6),
-            "6b6579",
-        );
+        assert_eq!(super::encode(b"key", &super::Digits(16), 6), "6b6579");
 
         // python -c 'import numpy; print(numpy.base_repr(int.from_bytes(b"key"), 23)[-6:])'
-        assert_eq!(
-            super::encode(b"key", ('0'..='9').chain('A'..='M'), 6),
-            "123AM7",
-        );
+        assert_eq!(super::encode(b"key", &super::Digits(23), 6), "123am7");
     }
 }
